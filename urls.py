4ced185f2e35a7457d14726e6c4769d1b3118ea2
Sandy Walsh <sandy.walsh@rackspace.com>
2012-02-28 21:35:07 -0600

    cleanup UI and add search

--- urls.py
+++ urls.py
@@ -1,9 +1,20 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    url(r'^', include('stacktach.url')),
+    url(r'^$', 'stacktach.views.welcome', name='welcome'),
+    url(r'new_tenant', 'stacktach.views.new_tenant', name='new_tenant'),
+    url(r'logout', 'stacktach.views.logout', name='logout'),
+    url(r'^(?P<tenant_id>\d+)/$', 'stacktach.views.home', name='home'),
+    url(r'^(?P<tenant_id>\d+)/data/$', 'stacktach.views.data',
+        name='data'),
+    url(r'^(?P<tenant_id>\d+)/details/(?P<column>\w+)/(?P<row_id>\d+)/$',
+        'stacktach.views.details', name='details'),
+    url(r'^(?P<tenant_id>\d+)/search/$',
+        'stacktach.views.search', name='search'),
+    url(r'^(?P<tenant_id>\d+)/expand/(?P<row_id>\d+)/$',
+        'stacktach.views.expand', name='expand'),
+    url(r'^(?P<tenant_id>\d+)/host_status/$', 
+        'stacktach.views.host_status', name='host_status'),
+    url(r'^(?P<tenant_id>\d+)/instance_status/$',
+        'stacktach.views.instance_status', name='instance_status'),
 )
