1eae8c45314f4094efcc0a9f0059da490dee3f62
Sandy Walsh <sandy@sandywalsh.com>
2013-02-19 12:26:07 -0400

    Reports by region

--- stacktach/models.py
+++ stacktach/models.py
@@ -20,6 +20,9 @@ from django.db import models
 class Deployment(models.Model):
     name = models.CharField(max_length=50)
 
+    def __repr__(self):
+        return self.name
+
 
 class RawData(models.Model):
     deployment = models.ForeignKey(Deployment)
