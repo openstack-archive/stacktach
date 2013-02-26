5b1c3b4c6070a592742ade2dc4ae347b3c167b22
Andrew Melton <andrew.melton@rackspace.com>
2013-02-26 17:15:53 -0500

    Adding received filter on exists api call

--- stacktach/dbapi.py
+++ stacktach/dbapi.py
@@ -74,21 +74,44 @@ def get_usage_delete(request, delete_id):
     return {'delete': _get_model_by_id(models.InstanceDeletes, delete_id)}
 
 
+def _exists_extra_values(exist):
+    values = {'received': str(dt.dt_from_decimal(exist.raw.when))}
+    return values
+
+
 @api_call
 def list_usage_exists(request):
-    objects = get_db_objects(models.InstanceExists, request, 'id')
-    dicts = _convert_model_list(objects)
+    try:
+        custom_filters = {}
+        if 'received_min' in request.GET:
+            received_min = request.GET['received_min']
+            custom_filters['received_min'] = {}
+            custom_filters['received_min']['raw__when__gte'] = \
+                utils.str_time_to_unix(received_min)
+        if 'received_max' in request.GET:
+            received_max = request.GET['received_max']
+            custom_filters['received_max'] = {}
+            custom_filters['received_max']['raw__when__lte'] = \
+                utils.str_time_to_unix(received_max)
+    except AttributeError:
+        msg = "Range filters must be dates."
+        raise BadRequestException(message=msg)
+
+    objects = get_db_objects(models.InstanceExists, request, 'id',
+                             custom_filters=custom_filters)
+    dicts = _convert_model_list(objects, _exists_extra_values)
     return {'exists': dicts}
 
 
 @api_call
 def get_usage_exist(request, exist_id):
-    return {'exist': _get_model_by_id(models.InstanceExists, exist_id)}
+    return {'exist': _get_model_by_id(models.InstanceExists, exist_id,
+                                      _exists_extra_values)}
 
 
-def _get_model_by_id(klass, model_id):
+def _get_model_by_id(klass, model_id, extra_values_func=None):
     model = get_object_or_404(klass, id=model_id)
-    model_dict = _convert_model(model)
+    model_dict = _convert_model(model, extra_values_func)
     return model_dict
 
 
@@ -100,7 +123,7 @@ def _check_has_field(klass, field_name):
         raise BadRequestException(msg)
 
 
-def _get_filter_args(klass, request):
+def _get_filter_args(klass, request, custom_filters=None):
     filter_args = {}
     if 'instance' in request.GET:
         uuid = request.GET['instance']
@@ -110,29 +133,36 @@ def _get_filter_args(klass, request):
             raise BadRequestException(msg)
 
     for (key, value) in request.GET.items():
-
-            if key.endswith('_min'):
-                k = key[0:-4]
-                _check_has_field(klass, k)
-                try:
-                    filter_args['%s__gte' % k] = utils.str_time_to_unix(value)
-                except AttributeError:
-                    msg = "Range filters must be dates."
-                    raise BadRequestException(message=msg)
-            elif key.endswith('_max'):
-                k = key[0:-4]
-                _check_has_field(klass, k)
-                try:
-                    filter_args['%s__lte' % k] = utils.str_time_to_unix(value)
-                except AttributeError:
-                    msg = "Range filters must be dates."
-                    raise BadRequestException(message=msg)
+            if not custom_filters or key not in custom_filters:
+                if key.endswith('_min'):
+                    k = key[0:-4]
+                    _check_has_field(klass, k)
+                    try:
+                        filter_args['%s__gte' % k] = \
+                            utils.str_time_to_unix(value)
+                    except AttributeError:
+                        msg = "Range filters must be dates."
+                        raise BadRequestException(message=msg)
+                elif key.endswith('_max'):
+                    k = key[0:-4]
+                    _check_has_field(klass, k)
+                    try:
+                        filter_args['%s__lte' % k] = \
+                            utils.str_time_to_unix(value)
+                    except AttributeError:
+                        msg = "Range filters must be dates."
+                        raise BadRequestException(message=msg)
 
     return filter_args
 
 
-def get_db_objects(klass, request, default_order_by, direction='asc'):
-    filter_args = _get_filter_args(klass, request)
+def get_db_objects(klass, request, default_order_by, direction='asc',
+                   custom_filters=None):
+    filter_args = _get_filter_args(klass, request,
+                                   custom_filters=custom_filters)
+    if custom_filters:
+        for key in custom_filters:
+            filter_args.update(custom_filters[key])
 
     if len(filter_args) > 0:
         objects = klass.objects.filter(**filter_args)
@@ -160,17 +190,19 @@ def get_db_objects(klass, request, default_order_by, direction='asc'):
     return objects.order_by(order_by)[start:end]
 
 
-def _convert_model(model):
+def _convert_model(model, extra_values_func=None):
     model_dict = model_to_dict(model)
     for key in model_dict:
         if isinstance(model_dict[key], decimal.Decimal):
             model_dict[key] = str(dt.dt_from_decimal(model_dict[key]))
+    if extra_values_func:
+        model_dict.update(extra_values_func(model))
     return model_dict
 
 
-def _convert_model_list(model_list):
+def _convert_model_list(model_list, extra_values_func=None):
     converted = []
     for item in model_list:
-        converted.append(_convert_model(item))
+        converted.append(_convert_model(item, extra_values_func))
 
     return converted
