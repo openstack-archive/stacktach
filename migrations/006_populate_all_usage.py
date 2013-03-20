06d9ea7d87d7572fff7f1198d91a9a592c18627f
Andrew Melton <andrew.melton@rackspace.com>
2013-03-20 13:45:48 -0400

    Determining a good start time.

--- migrations/006_populate_all_usage.py
+++ migrations/006_populate_all_usage.py
@@ -13,6 +13,7 @@ if os.path.exists(os.path.join(POSSIBLE_TOPDIR, 'stacktach')):
 
 from django.core.exceptions import MultipleObjectsReturned
 
+from stacktach import datetime_to_decimal as dt
 from stacktach import models
 from stacktach import views
 
@@ -57,28 +58,44 @@ def print_status(event, completed, errored, total):
     print "%s: %s completed, %s errored, %s remaining" % out
 
 
+def find_start_decimal():
+    usage = models.InstanceUsage.objects.all().order_by('id')[0]
+    usage_raw = models.RawData.objects.filter(request_id=usage.request_id)\
+                                      .order_by('when')[0]
+    delete = models.InstanceDeletes.objects.all().order_by('id')[0]
+    # Start a day after receiving the first usage, just to be safe
+    return min(usage_raw.when, delete.raw.when) + (60 * 60 * 24)
+
+start_decimal = find_start_decimal()
+
+print "Starting from date %s" % dt.dt_from_decimal(start_decimal)
+
 for event in events:
     start = datetime.datetime.utcnow()
 
-    raws = models.RawData.objects.filter(event=event)
+    raws = models.RawData.objects.filter(event=event, when__lte=start_decimal)
     total = raws.count()
     completed = 0
     errored = 0
+    loc = 0
 
     print_status(event, completed, errored, total)
     update_interval = datetime.timedelta(seconds=30)
     next_update = start + update_interval
-    for raw in raws:
-        try:
-            populate_usage(raw)
-            completed += 1
-        except Exception:
-            errored += 1
-            print "Error with raw: %s" % raw.id
+    while loc < total:
+        new_loc = loc + 500
+        for raw in raws[loc:new_loc]:
+            try:
+                populate_usage(raw)
+                completed += 1
+            except Exception:
+                errored += 1
+                print "Error with raw: %s" % raw.id
 
-        if datetime.datetime.utcnow() < next_update:
-            print_status(event, completed, errored, total)
-            next_update = datetime.datetime.utcnow() + update_interval
+            if datetime.datetime.utcnow() > next_update:
+                print_status(event, completed, errored, total)
+                next_update = datetime.datetime.utcnow() + update_interval
+        loc = new_loc
 
     end = datetime.datetime.utcnow()
     print_status(event, completed, errored, total)
