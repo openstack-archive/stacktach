79a7c8c4a19a7fcf92922c9db94607cfaf4b3ced
Andrew Melton <andrew.melton@rackspace.com>
2013-03-22 16:58:04 -0400

    Correcting 006_delta column creation

--- migrations/006_delta.sql
+++ migrations/006_delta.sql
@@ -1,2 +1,2 @@
-ALTER TABLE stacktach_instanceexists ADD fail_reason VARCHAR(200);
+ALTER TABLE stacktach_instanceexists ADD fail_reason VARCHAR(2000);
 CREATE INDEX `stacktach_instanceexists_fail_reason1` ON `stacktach_instanceexists` (`fail_reason`);
