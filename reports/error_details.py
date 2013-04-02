fcd31a4a3a43e794e78a6cc7920369dfa89fbd2c
Sandy Walsh <sandy@sandywalsh.com>
2013-04-02 19:10:42 -0300

    save to db

--- reports/error_details.py
+++ reports/error_details.py
@@ -27,18 +27,23 @@ if len(sys.argv) == 2:
         sys.exit(1)
 
 hours = 0
-length = 6
+length = 3
 
 start = datetime.datetime(year=yesterday.year, month=yesterday.month, 
                           day=yesterday.day) 
 end = start + datetime.timedelta(hours=length-1, minutes=59, seconds=59)
 
-print "Generating report for %s to %s" % (start, end)
+report = [{'raw_text':True}]  # Tell Stacky not to format results.
+report.append("Generating report for %s to %s" % (start, end))
 
 dstart = dt.dt_to_decimal(start)
 dend = dt.dt_to_decimal(end)
 
 codes = {}
+
+deployments = {}
+for deploy in models.Deployment.objects.all():
+    deployments[deploy.id] = deploy.name
 
 # Get all the instances that have changed in the last N hours ...
 updates = models.RawData.objects.filter(event='compute.instance.update',
@@ -72,7 +77,7 @@ for uuid_dict in updates:
                                              "deployment")\
                                      .order_by('when'))
 
-        start = None
+        _start = None
         err_id = None
         failure_type = None
 
@@ -95,8 +100,8 @@ for uuid_dict in updates:
             _name = raw['deployment']
             _id = raw['id']
 
-            if not start:
-                start = _when
+            if not _start:
+                _start = _when
 
             if 'error' in _routing_key:
                 err_id = _id
@@ -112,20 +117,18 @@ for uuid_dict in updates:
                 err_id = None
 
             if _tenant:
-                if tenant > 0 and _tenant != tenant:
-                    print "Conflicting tenant ID", _tenant, tenant
                 tenant = _tenant
 
             for cmd in cmds:
                 if cmd in _event:
                     operation = cmd
-                    cell = _name
+                    cell = deployments.get(_name, "n/a")
                     break
 
             if _image_type:
                 image_type_num |= _image_type
 
-        if not start:
+        if not _start:
             continue
 
         image = "?"
@@ -134,8 +137,8 @@ for uuid_dict in updates:
         if image_type.isset(image_type_num, image_type.SNAPSHOT_IMAGE):
             image = "snap"
 
-        end = _when
-        diff = end - start
+        _end = _when
+        diff = _end - _start
 
         if diff > 3600:
             failure_type = ">60"
@@ -156,12 +159,12 @@ for uuid_dict in updates:
             successes[key] = successes.get(key, 0) + 1
         else:
             err = models.RawData.objects.get(id=err_id)
-            print
-            print "------", uuid, "----------"
-            print "    Req:", req
-            print "    Duration: %.2f minutes" % (diff / 60)
-            print "    Operation:", operation
-            print "    Platform:", image_type.readable(image_type_num)
+            report.append('')
+            report.append("------ %s ----------" % uuid)
+            report.append("Req: %s" % req)
+            report.append("Duration: %.2f minutes" % (diff / 60))
+            report.append("Operation: %s" % operation)
+            report.append("Platform: %s" % image_type.readable(image_type_num))
             failures[key] = failures.get(key, 0) + 1
             tenant_issues[tenant] = tenant_issues.get(tenant, 0) + 1
 
@@ -169,18 +172,18 @@ for uuid_dict in updates:
                 queue, body = json.loads(err.json)
                 payload = body['payload']
                 
-                print "    Event ID:", err.id
-                print "    Tenant:", err.tenant
-                print "    Service:", err.service
-                print "    Host:", err.host
-                print "    Deployment:", err.deployment.name
-                print "    Event:", err.event
-                print "    When:", dt.dt_from_decimal(err.when)
+                report.append("Event ID: %s" % err.id)
+                report.append("Tenant: %s" % err.tenant)
+                report.append("Service: %s" % err.service)
+                report.append("Host: %s" % err.host)
+                report.append("Deployment: %s" % err.deployment.name)
+                report.append("Event: %s" % err.event)
+                report.append("When: %s" % dt.dt_from_decimal(err.when))
                 exc = payload.get('exception')
                 if exc:
                     # group the messages ...
                     exc_str = str(exc)
-                    print "    Exception:", exc_str
+                    report.append("Exception: %s" % exc_str)
                     error_messages[exc_str] = \
                                         error_messages.get(exc_str, 0) + 1
                     
@@ -189,17 +192,20 @@ for uuid_dict in updates:
                     if code:
                         codes[code] = codes.get(code, 0) + 1
                         failure_type = code
-                print "    Failure Type:", failure_type
+                report.append("Failure Type: %s" % failure_type)
 
-                print
-                print "Details:"
+                report.append('')
+                report.append("Details:")
                 raws = models.RawData.objects.filter(request_id=req)\
                                      .exclude(event='compute.instance.exists')\
                                      .order_by('when')
 
                 for raw in raws:
-                    print "H: %s E:%s, S:(%s->%s) T:(%s->%s)" % (raw.host, raw.event, 
-                                raw.old_state, raw.state, raw.old_task, raw.task)
+                    report.append("H: %s E:%s, S:(%s->%s) T:(%s->%s)" % 
+                                    (raw.host, raw.event, 
+                                     raw.old_state, raw.state, raw.old_task,
+                                     raw.task))
+                report.append('---------------------------------------')
             cause_key = (key, failure_type)
             causes[cause_key] = causes.get(cause_key, 0) + 1
 
@@ -208,13 +214,13 @@ def dump_breakdown(totals, label):
     p = prettytable.PrettyTable(["Category", "Count"])
     for k, v in totals.iteritems():
         p.add_row([k, v])
-    print label
+    report.append(label)
     p.sortby = 'Count'
-    print p
+    report.append(p.get_string())
 
 
 def dump_summary(info, label):
-    print "-- %s by operation by cell by platform --" % (label,)
+    report.append("-- %s by operation by cell by platform --" % (label,))
     p = prettytable.PrettyTable(["Operation", "Cell", "Platform", "Count",
                                  "Min", "Max", "Avg"])
     for c in ["Count", "Min", "Max", "Avg"]:
@@ -244,43 +250,42 @@ def dump_summary(info, label):
         p.add_row([operation, cell, text, count, _fmin, _fmax, _favg])
         total += count
     p.sortby = 'Count'
-    print p
+    report.append(p.get_string())
 
     dump_breakdown(op_totals, "Total %s by Operation" % label)
     dump_breakdown(cell_totals, "Total %s by Cell" % label)
     dump_breakdown(platform_totals, "Total %s by Platform" % label)
 
-    print
-
+    report.append('')
     return total
 
 
-print 
-print "SUMMARY"
-print
 good = dump_summary(successes, "Success")
 bad = dump_summary(failures, "Failures")
-print "====================================================="
-print "Total Success: %d Total Failure: %d" % (good, bad)
-print
+report.append(""" 
+SUMMARY
 
-print "-- Errors by Tenant --"
+=====================================================
+Total Success: %d Total Failure: %d
+
+""" % (good, bad))
+
 p = prettytable.PrettyTable(["Tenant", "Count"])
 for tenant, count in tenant_issues.iteritems():
     p.add_row([tenant, count])
 p.sortby = 'Count'
-print p
+report.append("""
+-- Errors by Tenant --
+%s""" % p.get_string())
 
-print
-print "-- Return code counts --"
 p = prettytable.PrettyTable(["Return Code", "Count"])
 for k, v in codes.iteritems():
     p.add_row([k, v])
 p.sortby = 'Count'
-print p
+report.append("""
+-- Return code counts --
+%s""" % p.get_string())
 
-print
-print "-- Cause breakdown --"
 p = prettytable.PrettyTable(["Cause", "Operation", "Cell", "Platform", "Count"])
 for cause_key, count in causes.iteritems():
     key, cause = cause_key
@@ -291,13 +296,26 @@ for cause_key, count in causes.iteritems():
         text = ", ".join(readable)
     p.add_row([cause, operation, cell, text, count])
 p.sortby = 'Count'
-print p
+report.append("""
+-- Cause breakdown --
+%s""" % p.get_string())
 
-print
-print "-- Error Message Counts --"
 p = prettytable.PrettyTable(["Count", "Message"])
 for k, v in error_messages.iteritems():
     p.add_row([v, k[:80]])
 p.sortby = 'Count'
-print p
+report.append("""
+-- Error Message Counts --
+%s""" % p.get_string())
 
+for r in report[1:]:
+    print r
+
+values = {'json': json.dumps(report),
+          'created': dt.dt_to_decimal(datetime.datetime.utcnow()),
+          'period_start': start,
+          'period_end': end,
+          'version': 1,
+          'name': 'Error detail report'}
+report = models.JsonReport(**values)
+report.save()
