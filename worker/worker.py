c759a48e596cfb263608dbacc5a5f6b58e22eddf
Thomas Maddox <thomas.maddox@RACKSPACE.com>
2013-05-23 14:11:22 -0500

    log failed message and raise to kill worker

--- worker/worker.py
+++ worker/worker.py
@@ -89,6 +89,7 @@ class NovaConsumer(kombu.mixins.ConsumerMixin):
 
         # save raw and ack the message
         raw = views.process_raw_data(self.deployment, args, asJson)
+
         if raw:
             self.processed += 1
             message.ack()
@@ -128,7 +129,10 @@ class NovaConsumer(kombu.mixins.ConsumerMixin):
         try:
             self._process(message)
         except Exception, e:
-            LOG.exception("Problem %s" % e)
+            LOG.debug("Problem: %s\nFailed message body:\n%s" %
+                      (e, json.loads(str(message.body)))
+                      )
+            raise
 
 
 def continue_running():
