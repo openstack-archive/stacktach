864809b3c560f31d4e18ceb0dad20e0414d017f5
Andrew Melton <andrew.melton@rackspace.com>
2013-06-18 14:47:27 -0400

    Erasing coverage datafile on each run

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nosetests tests --exclude-dir=stacktach --with-coverage --cover-package=stacktach,worker,verifier
+nosetests tests --exclude-dir=stacktach --with-coverage --cover-package=stacktach,worker,verifier --cover-erase
