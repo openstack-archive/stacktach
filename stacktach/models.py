22582fc5a211862106a65605e888ba98b9d3e72e
Manali Latkar <manalil@thoughtworks.com>
2014-03-21 15:20:33 +0530

    Adding host and deployment info to missing exists entries in the nova usage audit
    Change-Id: Ib8fc820859f281d24c38b714cd85d42964d17a59

--- stacktach/models.py
+++ stacktach/models.py
@@ -175,11 +175,14 @@ class InstanceUsage(models.Model):
     rax_options = models.TextField(null=True, blank=True)
 
     def deployment(self):
-        raws = RawData.objects.filter(request_id=self.request_id)
-        if raws.count() == 0:
-            return False
-        raw = raws[0]
-        return raw.deployment
+        return self.latest_raw_for_request_id().deployment.name
+
+    def latest_raw_for_request_id(self):
+        return RawData.objects.filter(
+            request_id=self.request_id).order_by('-id')[0]
+
+    def host(self):
+        return self.latest_raw_for_request_id().host
 
     @staticmethod
     def find(instance, launched_at):
