003c736f989563863386aab1949683913bba0346
Anuj Mathur <anuj.mathur@rackspace.com>
2014-03-28 11:09:42 +0530

    Added column headers for host and deployment in json reports
    Change-Id: I0304e3a0bf25ebe7f932cef1051649b66eff5eea

--- reports/nova_usage_audit.py
+++ reports/nova_usage_audit.py
@@ -254,7 +254,8 @@ def store_results(start, end, summary, details):
 
 def make_json_report(summary, details):
     report = [{'summary': summary},
-              ['Object', 'ID', 'Error Description', 'Reconciled?']]
+              ['Object', 'ID', 'Error Description', 'Reconciled?', 'Cell',
+               'Deployment']]
     report.extend(details['exist_fails'])
     report.extend(details['launch_fails'])
     return json.dumps(report)
